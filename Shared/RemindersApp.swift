61caf2255bd857d0953b9ef50a1ee6607aabd7c0
lcs-dsmith <dsmith@lcs.on.ca>
2021-03-18 08:58:12 -0400

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Dylan Smith on 2021-03-18.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
